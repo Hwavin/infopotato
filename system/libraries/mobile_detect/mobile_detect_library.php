79751bfe10a4597ff71e2fd42f7b2a25eba1aa53
Zhou Yuan <yuanzhou19@gmail.com>
2011-05-10 09:50:56 -0400

    mobile_detect library updated

--- system/libraries/mobile_detect/mobile_detect_library.php
+++ system/libraries/mobile_detect/mobile_detect_library.php
@@ -4,7 +4,7 @@
  * Mobile Detect
  *
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
- * @link       http://code.google.com/p/php-mobile-detect/
+ * @link       http://code.google.com/p/php-mobile-detect/issues/detail?id=11
  * @version    SVN: $Id: Mobile_Detect.php 3 2009-05-21 13:06:28Z vic.stanciu $
  */
 
@@ -20,21 +20,24 @@ class Mobile_Detect_Library {
     protected $is_palm       = NULL;
     protected $is_windows    = NULL;
     protected $is_generic    = NULL;
+	protected $is_iphone     = NULL;
+    protected $is_ipad       = NULL;
 
     protected $devices = array(
-        "android"       => "android",
-        "blackberry"    => "blackberry",
-        "iphone"        => "(iphone|ipod)",
-        "opera"         => "opera mini",
-        "palm"          => "(avantgo|blazer|elaine|hiptop|palm|plucker|xiino)",
-        "windows"       => "windows ce; (iemobile|ppc|smartphone)",
-        "generic"       => "(kindle|mobile|mmp|midp|o2|pda|pocket|psp|symbian|smartphone|treo|up.browser|up.link|vodafone|wap)"
+        'android'       => 'android',
+        'blackberry'    => 'blackberry',
+        'iphone'        => '(iphone|ipod)',
+		'ipad'          => 'ipad',
+        'opera'         => '(opera mini|opera mobi)',
+        'palm'          => '(avantgo|blazer|elaine|hiptop|palm|plucker|xiino)',
+        'windows'       => 'windows ce; (iemobile|ppc|smartphone)',
+        'generic'       => '(kindle|mobile|mmp|midp|o2|pda|pocket|psp|symbian|smartphone|treo|up.browser|up.link|vodafone|wap|nokia|samsung|SonyEricsson)'
     );
 
 
     public function __construct() {
         $this->user_agent = $_SERVER['HTTP_USER_AGENT'];
-        $this->accept    = $_SERVER['HTTP_ACCEPT'];
+        $this->accept     = $_SERVER['HTTP_ACCEPT'];
 
         if (isset($_SERVER['HTTP_X_WAP_PROFILE']) || isset($_SERVER['HTTP_PROFILE'])) {
             $this->is_mobile = TRUE;
@@ -42,7 +45,7 @@ class Mobile_Detect_Library {
             $this->is_mobile = TRUE;
         } else {
             foreach ($this->devices as $device => $regexp) {
-                if ($this->is_device($device)) {
+                if ($this->_is_device($device)) {
                     $this->is_mobile = TRUE;
                 }
             }
@@ -51,16 +54,16 @@ class Mobile_Detect_Library {
 
 
     /**
-     * Overloads is_android() | is_blackberry() | is_opera() | is_palm() | is_windows() | is_generic() through is_device()
+     * Overloads is_android() | is_blackberry() | is_opera() | is_palm() | is_windows() | is_generic() through _is_device()
      *
      * @param string $name
      * @param array $arguments
      * @return bool
      */
     public function __call($name, $arguments) {
-        $device = substr($name, 2);
-        if ($name == "is_" . strtolower($device)) {
-            return $this->is_device($device);
+		$device = strtolower(substr($name, 2));
+        if ($name == 'is_' . strtolower($device)) {
+            return $this->_is_device($device);
         } else {
             trigger_error("Method $name not defined", E_USER_ERROR);
         }
@@ -68,7 +71,10 @@ class Mobile_Detect_Library {
 
 
     /**
-     * Returns true if any type of mobile device detected, including special ones
+     * To be used if you are only interested in checking to see if the user is using a mobile device, 
+	 * without caring for specific platform
+	 * 
+	 * Returns true if any type of mobile device detected, including special ones
      * @return bool
      */
     public function is_mobile() {
@@ -76,11 +82,11 @@ class Mobile_Detect_Library {
     }
 
 
-    protected function is_device($device) {
-        $var    = "is_" . strtolower($device);
+    protected function _is_device($device) {
+        $var    = 'is_' . strtolower($device);
         $return = $this->$var === null ? (bool) preg_match("/" . $this->devices[$device] . "/i", $this->user_agent) : $this->$var;
 
-        if ($device != 'generic' && $return == TRUE) {
+        if (($device != 'generic' && $return == TRUE) || $device == 'ipad') {
             $this->is_generic = FALSE;
         }
 
