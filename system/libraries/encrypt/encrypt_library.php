0dccb0f15c22b84cb56d1f6e693325cf571bc930
Zhou Yuan <yuanzhou19@gmail.com>
2013-03-21 16:05:05 -0400

    removed cart and feed_writer libaray

--- system/libraries/encrypt/encrypt_library.php
+++ system/libraries/encrypt/encrypt_library.php
@@ -5,7 +5,7 @@
  * 
  * @author Zhou Yuan <yuanzhou19@gmail.com>
  * @link http://www.infopotato.com/
- * @copyright Copyright &copy; 2009-2012 Zhou Yuan
+ * @copyright Copyright &copy; 2009-2013 Zhou Yuan
  * @license http://www.opensource.org/licenses/mit-license.php MIT Licence
  */
 class Encrypt_Library {
