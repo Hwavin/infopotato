1cb71ed7b10e483d5180a671da8928cb3628d469
Zhou Yuan <yuanzhou19@gmail.com>
2013-04-16 09:29:45 -0400

    removed the understore prefix for private function

--- system/core/data.php
+++ system/core/data.php
@@ -26,7 +26,7 @@ class Data {
 	 */
 	public function __construct($connection = '') {
 		if ($connection !== '') {
-			$this->db = self::_create_db_obj($connection);
+			$this->db = self::create_db_obj($connection);
 		}
 	}
 	
@@ -36,7 +36,7 @@ class Data {
 	 * @param	string $connection database connection pool, e.g., 'mysql_dao:default'
 	 * @return	a specific database access object
 	 */
-	private static function _create_db_obj($connection) {
+	private static function create_db_obj($connection) {
 		static $db_obj = array();
 		
 		if (isset($db_obj[$connection])) {
