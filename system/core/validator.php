0abd65f7afdbdb9ed5671572b863af436b7da24b
Zhou Yuan <yuanzhou19@gmail.com>
2013-06-25 10:55:19 -0400

    updated valid_date() in validator

--- system/core/validator.php
+++ system/core/validator.php
@@ -144,6 +144,57 @@ class Validator {
         return ! empty($input) && preg_match('/^[+]?([\d]{0,3})?[\(\.\-\s]?([\d]{1,3})[\)\.\-\s]*(([\d]{3})[\.\-\s]?([\d]{4})|([\d]{2}[\.\-\s]?){4})$/', $str);
     }
     
+        
+    /**
+     *
+     * Validate a date
+     *
+     * @param    string    $date
+     * @param    string    format
+     * @return    bool
+     *
+     */
+    function valid_date($date, $format = 'YYYY-MM-DD') {
+        switch($format) {
+            case 'YYYY/MM/DD':
+            case 'YYYY-MM-DD':
+                list($y, $m, $d) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'YYYY/DD/MM':
+            case 'YYYY-DD-MM':
+                list($y, $d, $m) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'DD-MM-YYYY':
+            case 'DD/MM/YYYY':
+                list($d, $m, $y) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'MM-DD-YYYY':
+            case 'MM/DD/YYYY':
+                list($m, $d, $y) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'YYYYMMDD':
+                $y = substr($date, 0, 4);
+                $m = substr($date, 4, 2);
+                $d = substr($date, 6, 2);
+                break;
+
+            case 'YYYYDDMM':
+                $y = substr($date, 0, 4);
+                $d = substr($date, 4, 2);
+                $m = substr($date, 6, 2);
+                break;
+
+            default:
+                throw new Exception("Invalid Date Format");
+        }
+        
+        return checkdate($m, $d, $y);
+    }
+    
     /**
      * Alpha
      *
@@ -238,56 +289,7 @@ class Validator {
         }
         return TRUE;
     }
-    
-    /**
-     *
-     * Validate a date
-     *
-     * @param    string    $date
-     * @param    string    format
-     * @return    bool
-     *
-     */
-    function valid_date($date, $format = 'YYYY-MM-DD') {
-        switch($format) {
-            case 'YYYY/MM/DD':
-            case 'YYYY-MM-DD':
-                list($y, $m, $d) = preg_split('/[-\.\/ ]/', $date);
-                break;
 
-            case 'YYYY/DD/MM':
-            case 'YYYY-DD-MM':
-                list($y, $d, $m) = preg_split('/[-\.\/ ]/', $date);
-                break;
-
-            case 'DD-MM-YYYY':
-            case 'DD/MM/YYYY':
-                list($d, $m, $y) = preg_split('/[-\.\/ ]/', $date);
-                break;
-
-            case 'MM-DD-YYYY':
-            case 'MM/DD/YYYY':
-                list($m, $d, $y) = preg_split('/[-\.\/ ]/', $date);
-                break;
-
-            case 'YYYYMMDD':
-                $y = substr($date, 0, 4);
-                $m = substr($date, 4, 2);
-                $d = substr($date, 6, 2);
-                break;
-
-            case 'YYYYDDMM':
-                $y = substr($date, 0, 4);
-                $d = substr($date, 4, 2);
-                $m = substr($date, 6, 2);
-                break;
-
-            default:
-                throw new Exception("Invalid Date Format");
-        }
-        
-        return checkdate($m, $d, $y);
-    }
 
 }
 
