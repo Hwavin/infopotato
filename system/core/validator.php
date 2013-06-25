0bf8397600b18f19fc6607757a1d0732b29e762f
Zhou Yuan <yuanzhou19@gmail.com>
2013-06-25 10:50:26 -0400

    updated valid_date() in validator

--- system/core/validator.php
+++ system/core/validator.php
@@ -240,22 +240,53 @@ class Validator {
     }
     
     /**
-     * Is a valid date?
      *
-     * @param    string
-     * @param    string
+     * Validate a date
+     *
+     * @param    string    $date
+     * @param    string    format
      * @return    bool
+     *
      */
-    public static function valid_date($str, $format = NULL) {
-        if ($str instanceof DateTime) {
-            return TRUE;
-        } elseif (is_null($format)) {
-            return FALSE !== strtotime($str);
+    function valid_date($date, $format = 'YYYY-MM-DD') {
+        switch($format) {
+            case 'YYYY/MM/DD':
+            case 'YYYY-MM-DD':
+                list($y, $m, $d) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'YYYY/DD/MM':
+            case 'YYYY-DD-MM':
+                list($y, $d, $m) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'DD-MM-YYYY':
+            case 'DD/MM/YYYY':
+                list($d, $m, $y) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'MM-DD-YYYY':
+            case 'MM/DD/YYYY':
+                list($m, $d, $y) = preg_split('/[-\.\/ ]/', $date);
+                break;
+
+            case 'YYYYMMDD':
+                $y = substr($date, 0, 4);
+                $m = substr($date, 4, 2);
+                $d = substr($date, 6, 2);
+                break;
+
+            case 'YYYYDDMM':
+                $y = substr($date, 0, 4);
+                $d = substr($date, 4, 2);
+                $m = substr($date, 6, 2);
+                break;
+
+            default:
+                throw new Exception("Invalid Date Format");
         }
-
-        $dateFromFormat = DateTime::createFromFormat($format, $str);
-
-        return $dateFromFormat && $str === date($format, $dateFromFormat->getTimestamp());
+        
+        return checkdate($m, $d, $y);
     }
 
 }
