9c062ddc43c5a83c5ab766d0838198c7894c5092
Zhou Yuan <yuanzhou19@gmail.com>
2013-07-02 11:32:56 -0400

    updated comments

--- system/libraries/email/email_library.php
+++ system/libraries/email/email_library.php
@@ -1782,8 +1782,8 @@ class Email_Library {
         }
         
         // If no transport is specified, tcp:// will be assumed
-        // The ssl:// and tls:// transports (available only when openssl support is compiled into PHP) 
-        // are extensions of the tcp:// transport which include SSL encryption.
+        // If OpenSSL support is installed, you may prefix the hostname with either ssl:// or tls:// 
+        // to use an SSL or TLS client connection over TCP/IP to connect to the remote host.
         // ssl:// will attempt to negotiate an SSL V2, or SSL V3 connection 
         // depending on the capabilities and preferences of the remote host.
         $ssl = ($this->smtp_crypto === 'ssl') ? 'ssl://' : '';
