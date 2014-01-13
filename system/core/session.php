497eb42f306b96157338bb037785c12df3e7910b
Zhou Yuan <yuanzhou19@gmail.com>
2014-01-13 10:43:49 -0500

    removed session array dereferencing since function array dereferencing added in PHP 5.4

--- system/core/session.php
+++ system/core/session.php
@@ -122,112 +122,39 @@ class Session {
     public static function set($key, $value) {
         self::open();
 
-        $tip =& $_SESSION;
-        
-        // Array elements can be modified via '[sub-key]' syntax, and thus '[' and ']' can not be used in key names
-        if ($bracket_pos = strpos($key, '[')) {
-            $array_dereference = substr($key, $bracket_pos);
-            $key = substr($key, 0, $bracket_pos);
-            
-            preg_match_all('#(?<=\[)[^\[\]]+(?=\])#', $array_dereference, $array_keys, PREG_SET_ORDER);
-            $array_keys = array_map('current', $array_keys);
-            array_unshift($array_keys, $key);
-            
-            foreach (array_slice($array_keys, 0, -1) as $array_key) {
-                if ( ! isset($tip[$array_key]) || ! is_array($tip[$array_key])) {
-                    $tip[$array_key] = array();
-                }
-                $tip =& $tip[$array_key];
-            }
-
-            $tip[end($array_keys)] = $value;        
-        } else {
-            $tip[$key] = $value;
-        }
+        $_SESSION[$key] = $value;
     }
 
 
     /**
      * Gets data from the $_SESSION superglobal
      * 
-     * @param string $key The name to get the value for - array elements can be accessed via `[sub-key]` syntax, and thus `[` and `]` can not be used in key names
+     * @param string $key The name to get the value for
      * @param mixed  $default_value The default value to use if the requested key is not set
      * @return mixed The data element requested
      */
     public static function get($key, $default_value = NULL) {
         self::open();
-        
-        $array_dereference = NULL;
-        if ($bracket_pos = strpos($key, '[')) {
-            $array_dereference = substr($key, $bracket_pos);
-            $key = substr($key, 0, $bracket_pos);
-        }
-        
-        if ( ! isset($_SESSION[$key])) {
-            return $default_value;
-        }
-        $value = $_SESSION[$key];
-        
-        if ($array_dereference) {
-            preg_match_all('#(?<=\[)[^\[\]]+(?=\])#', $array_dereference, $array_keys, PREG_SET_ORDER);
-            $array_keys = array_map('current', $array_keys);
-            
-            foreach ($array_keys as $array_key) {
-                if ( ! is_array($value) || ! isset($value[$array_key])) {
-                    $value = $default_value;
-                    break;
-                }
-                $value = $value[$array_key];
-            }
-        }
-        
-        return $value;
+
+        return (isset($_SESSION[$key])) ? $_SESSION[$key] : $default_value;
     }
 
         
     /**
      * Deletes a value from the session
      * 
-     * @param string $key The key of the value to delete - array elements can be modified via `[sub-key]` syntax, and thus `[` and `]` can not be used in key names
-     * @param mixed  $default_value The value to return if the `$key` is not set
-     * @return mixed The value of the `$key` that was deleted
+     * @param string $key The key of the value to delete
+     * @param mixed  $default_value The value to return if the key is not set
+     * @return mixed The value of the key that was deleted
      */
     public static function delete($key, $default_value = NULL) {
         self::open();
         
         $value = $default_value;
-        
-        if ($bracket_pos = strpos($key, '[')) {
-            $original_key = $key;
-            $array_dereference = substr($key, $bracket_pos);
-            $key = substr($key, 0, $bracket_pos);
-            
-            if ( ! isset($_SESSION[$key])) {
-                return $value;
-            }
-            
-            preg_match_all('#(?<=\[)[^\[\]]+(?=\])#', $array_dereference, $array_keys, PREG_SET_ORDER);
-            $array_keys = array_map('current', $array_keys);
-            
-            $tip =& $_SESSION[$key];
-            
-            foreach (array_slice($array_keys, 0, -1) as $array_key) {
-                if ( ! isset($tip[$array_key])) {
-                    return $value;
-                } elseif ( ! is_array($tip[$array_key])) {
-                    Common::halt('A System Error Was Encountered', "Session::delete() was called for an element, {$original_key}, which is not an array", 'sys_error');
-                }
-                $tip =& $tip[$array_key];
-            }
-            
-            $key = end($array_keys);
-        } else {
-            $tip =& $_SESSION;
-        }
-        
-        if (isset($tip[$key])) {
-            $value = $tip[$key];
-            unset($tip[$key]);
+
+        if (isset($_SESSION[$key])) {
+            $value = $_SESSION[$key];
+            unset($_SESSION[$key]);
         }
         
         return $value;
