11e1a9b17c6bf10a31c81fafc8c4cba5f72a2017
Zhou Yuan <yuanzhou19@gmail.com>
2014-04-08 15:09:38 -0400

    Added Status-Line in response()

--- system/core/manager.php
+++ system/core/manager.php
@@ -81,13 +81,94 @@ class Manager {
      * 
      * $config['content']: (string required) content to output
      * $config['type']: (string required)  specify the media type for the output
+     * $config['status']: (int optional)  specify the Status-Line
      * $config['extra_headers']: (array optional)  any extra headers to response
      * 
      * @return void
      */   
     protected function response(array $config = NULL) {
         if (isset($config['content']) && isset($config['type'])) {
-            // Response headers
+            // Send the Status-Line (case is not significant) if specified
+            // In most cases, no need to specify this, the server will use "HTTP/1.1 200 OK"
+            if (isset($config['status'])) {
+                // Mapping of status codes to reason phrases
+                $status_map = array(
+                    // 1xx: Informational - Request received, continuing process
+                    100 => 'Continue',
+                    101 => 'Switching Protocols',
+                    102 => 'Processing',
+                    // 2xx: Success - The action was successfully received, understood, and accepted
+                    200 => 'OK',
+                    201 => 'Created',
+                    202 => 'Accepted',
+                    203 => 'Non-Authoritative Information',
+                    204 => 'No Content',
+                    205 => 'Reset Content',
+                    206 => 'Partial Content',
+                    207 => 'Multi-Status',
+                    208 => 'Already Reported',
+                    226 => 'IM Used',
+                    // 3xx: Redirection - Further action must be taken in order to complete the request
+                    300 => 'Multiple Choices',
+                    301 => 'Moved Permanently',
+                    302 => 'Found',
+                    303 => 'See Other',
+                    304 => 'Not Modified',
+                    305 => 'Use Proxy',
+                    307 => 'Temporary Redirect',
+                    308 => 'Permanent Redirect',
+                    // 4xx: Client Error - The request contains bad syntax or cannot be fulfilled
+                    400 => 'Bad Request',
+                    401 => 'Unauthorized',
+                    402 => 'Payment Required',
+                    403 => 'Forbidden',
+                    404 => 'Not Found',
+                    405 => 'Method Not Allowed',
+                    406 => 'Not Acceptable',
+                    407 => 'Proxy Authentication Required',
+                    408 => 'Request Timeout',
+                    409 => 'Conflict',
+                    410 => 'Gone',
+                    411 => 'Length Required',
+                    412 => 'Precondition Failed',
+                    413 => 'Request Entity Too Large',
+                    414 => 'Request-URI Too Long',
+                    415 => 'Unsupported Media Type',
+                    416 => 'Requested Range Not Satisfiable',
+                    417 => 'Expectation Failed',
+                    422 => 'Unprocessable Entity',
+                    423 => 'Locked',
+                    424 => 'Failed Dependency',
+                    425 => 'Reserved for WebDAV advanced collections expired proposal',
+                    426 => 'Upgrade required',
+                    428 => 'Precondition Required',
+                    429 => 'Too Many Requests',
+                    431 => 'Request Header Fields Too Large',
+                    // 5xx: Server Error - The server failed to fulfill an apparently valid request
+                    500 => 'Internal Server Error',
+                    501 => 'Not Implemented',
+                    502 => 'Bad Gateway',
+                    503 => 'Service Unavailable',
+                    504 => 'Gateway Timeout',
+                    505 => 'HTTP Version Not Supported',
+                    506 => 'Variant Also Negotiates (Experimental)',
+                    507 => 'Insufficient Storage',
+                    508 => 'Loop Detected',
+                    510 => 'Not Extended',
+                    511 => 'Network Authentication Required',
+                );
+                
+                if (isset($status_map[$config['status']])) {
+                    // Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+                    $status_line = 'HTTP/1.1 '.$config['status'].' '.$status_map[$config['status']];
+                    // Replace the default Status-Line
+                    header($status_line);
+                } else {
+                    Logger::log_debug(APP_LOG_DIR, 'The provided HTTP status code is invalid.');
+                }
+            }
+            
+            // Response Header Fields
             // The origin server will add additional headers about the response if not specified here
             $headers = array();
             
