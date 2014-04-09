3cb1ac8bbd605c3a5d6a3c0eab1d4ebe8ff0022f
Zhou Yuan <yuanzhou19@gmail.com>
2014-04-09 11:53:23 -0400

    refactored response()

--- system/core/manager.php
+++ system/core/manager.php
@@ -79,125 +79,137 @@ class Manager {
      * 
      * @param array $config options
      * 
-     * $config['content']: (string required) content to output
-     * $config['type']: (string required)  specify the media type for the output
-     * $config['status']: (int optional)  specify the Status-Line
-     * $config['extra_headers']: (array optional)  any extra headers to response
+     * $config['content']: (string, optional for 4xx, no content for 201, 204, 304) content to output
+     * $config['type']: (string, optional, suggested to specify if there's output content)  specify the media type for the output
+     * $config['status']: (int, optional, use if only you want to change the default status code)  specify the Status-Line
+     * $config['extra_headers']: (array, optional)  any extra headers to response
      * 
      * @return void
      */   
     protected function response(array $config = NULL) {
-        if (isset($config['content']) && isset($config['type'])) {
-            // Send the Status-Line (case is not significant) if specified
-            // In most cases, no need to specify this, the server will use "HTTP/1.1 200 OK"
-            if (isset($config['status'])) {
-                // Mapping of status codes to reason phrases
-                $status_map = array(
-                    // 1xx: Informational - Request received, continuing process
-                    100 => 'Continue',
-                    101 => 'Switching Protocols',
-                    102 => 'Processing',
-                    // 2xx: Success - The action was successfully received, understood, and accepted
-                    200 => 'OK',
-                    201 => 'Created',
-                    202 => 'Accepted',
-                    203 => 'Non-Authoritative Information',
-                    204 => 'No Content',
-                    205 => 'Reset Content',
-                    206 => 'Partial Content',
-                    207 => 'Multi-Status',
-                    208 => 'Already Reported',
-                    226 => 'IM Used',
-                    // 3xx: Redirection - Further action must be taken in order to complete the request
-                    300 => 'Multiple Choices',
-                    301 => 'Moved Permanently',
-                    302 => 'Found',
-                    303 => 'See Other',
-                    304 => 'Not Modified',
-                    305 => 'Use Proxy',
-                    307 => 'Temporary Redirect',
-                    308 => 'Permanent Redirect',
-                    // 4xx: Client Error - The request contains bad syntax or cannot be fulfilled
-                    400 => 'Bad Request',
-                    401 => 'Unauthorized',
-                    402 => 'Payment Required',
-                    403 => 'Forbidden',
-                    404 => 'Not Found',
-                    405 => 'Method Not Allowed',
-                    406 => 'Not Acceptable',
-                    407 => 'Proxy Authentication Required',
-                    408 => 'Request Timeout',
-                    409 => 'Conflict',
-                    410 => 'Gone',
-                    411 => 'Length Required',
-                    412 => 'Precondition Failed',
-                    413 => 'Request Entity Too Large',
-                    414 => 'Request-URI Too Long',
-                    415 => 'Unsupported Media Type',
-                    416 => 'Requested Range Not Satisfiable',
-                    417 => 'Expectation Failed',
-                    422 => 'Unprocessable Entity',
-                    423 => 'Locked',
-                    424 => 'Failed Dependency',
-                    425 => 'Reserved for WebDAV advanced collections expired proposal',
-                    426 => 'Upgrade required',
-                    428 => 'Precondition Required',
-                    429 => 'Too Many Requests',
-                    431 => 'Request Header Fields Too Large',
-                    // 5xx: Server Error - The server failed to fulfill an apparently valid request
-                    500 => 'Internal Server Error',
-                    501 => 'Not Implemented',
-                    502 => 'Bad Gateway',
-                    503 => 'Service Unavailable',
-                    504 => 'Gateway Timeout',
-                    505 => 'HTTP Version Not Supported',
-                    506 => 'Variant Also Negotiates (Experimental)',
-                    507 => 'Insufficient Storage',
-                    508 => 'Loop Detected',
-                    510 => 'Not Extended',
-                    511 => 'Network Authentication Required',
-                );
+        // Send the Status-Line (case is not significant) if specified
+        // In most cases, no need to specify this, the server will use "HTTP/1.1 200 OK"
+        if (isset($config['status'])) {
+            // Mapping of status codes to reason phrases
+            $status_map = array(
+                // 1xx: Informational - Request received, continuing process
+                100 => 'Continue',
+                101 => 'Switching Protocols',
+                102 => 'Processing',
+                // 2xx: Success - The action was successfully received, understood, and accepted
+                200 => 'OK',
+                201 => 'Created',
+                202 => 'Accepted',
+                203 => 'Non-Authoritative Information',
+                204 => 'No Content',
+                205 => 'Reset Content',
+                206 => 'Partial Content',
+                207 => 'Multi-Status',
+                208 => 'Already Reported',
+                226 => 'IM Used',
+                // 3xx: Redirection - Further action must be taken in order to complete the request
+                300 => 'Multiple Choices',
+                301 => 'Moved Permanently',
+                302 => 'Found',
+                303 => 'See Other',
+                304 => 'Not Modified',
+                305 => 'Use Proxy',
+                307 => 'Temporary Redirect',
+                308 => 'Permanent Redirect',
+                // 4xx: Client Error - The request contains bad syntax or cannot be fulfilled
+                400 => 'Bad Request',
+                401 => 'Unauthorized',
+                402 => 'Payment Required',
+                403 => 'Forbidden',
+                404 => 'Not Found',
+                405 => 'Method Not Allowed',
+                406 => 'Not Acceptable',
+                407 => 'Proxy Authentication Required',
+                408 => 'Request Timeout',
+                409 => 'Conflict',
+                410 => 'Gone',
+                411 => 'Length Required',
+                412 => 'Precondition Failed',
+                413 => 'Request Entity Too Large',
+                414 => 'Request-URI Too Long',
+                415 => 'Unsupported Media Type',
+                416 => 'Requested Range Not Satisfiable',
+                417 => 'Expectation Failed',
+                422 => 'Unprocessable Entity',
+                423 => 'Locked',
+                424 => 'Failed Dependency',
+                425 => 'Reserved for WebDAV advanced collections expired proposal',
+                426 => 'Upgrade required',
+                428 => 'Precondition Required',
+                429 => 'Too Many Requests',
+                431 => 'Request Header Fields Too Large',
+                // 5xx: Server Error - The server failed to fulfill an apparently valid request
+                500 => 'Internal Server Error',
+                501 => 'Not Implemented',
+                502 => 'Bad Gateway',
+                503 => 'Service Unavailable',
+                504 => 'Gateway Timeout',
+                505 => 'HTTP Version Not Supported',
+                506 => 'Variant Also Negotiates (Experimental)',
+                507 => 'Insufficient Storage',
+                508 => 'Loop Detected',
+                510 => 'Not Extended',
+                511 => 'Network Authentication Required',
+            );
+            
+            // In case the status code is quoted
+            $config['status'] = (int) $config['status'];
+
+            if (isset($status_map[$config['status']])) {
+                // Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+                $status_line = 'HTTP/1.1 '.$config['status'].' '.$status_map[$config['status']];
+                // Replace the default Status-Line
+                header($status_line);
                 
-                if (isset($status_map[$config['status']])) {
-                    // Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
-                    $status_line = 'HTTP/1.1 '.$config['status'].' '.$status_map[$config['status']];
-                    // Replace the default Status-Line
-                    header($status_line);
-                } else {
-                    Logger::log_debug(APP_LOG_DIR, 'The provided HTTP status code is invalid.');
+                // Determine if continue to output the message body
+                if (in_array($config['status'], array(201, 204, 304))) {
+                    // Send other response headers if provided
+                    // There are still headers added by the origin server
+                    if (isset($config['extra_headers']) && is_array($config['extra_headers'])) {
+                        // Send out all added extra headers before exit
+                        foreach ($config['extra_headers'] as $name => $val) {
+                            header($name.': '.$val);
+                        }
+                    }
+
+                    // These status codes must not be followed by a message body
+                    exit;
                 }
+            } else {
+                Logger::log_debug(APP_LOG_DIR, 'The provided HTTP status code is invalid.');
             }
-            
-            // Response Header Fields
-            // The origin server will add additional headers about the response if not specified here
-            $headers = array();
-            
-            // http://tools.ietf.org/html/rfc6657
-            // Common Textual Media Types that SHOULD/RECOMMENDED utf-8 charset encoding
-            // CSS: the majority of characters will be CSS syntax and thus US-ASCII, no need to force UTF-8 here
-            // XML: Although listed as an optional parameter, the use of the charset parameter is STRONGLY RECOMMENDED
-            // JSON: text SHALL be encoded in Unicode.  The default encoding is UTF-8.
-            // http://www.ietf.org/rfc/rfc4627.txt?number=4627
-            // JavaScript: http://www.rfc-editor.org/rfc/rfc4329.txt
-
-            // Any HTTP/1.1 message containing an entity-body SHOULD include a
-            // Content-Type header field defining the media type of that body.
-            $headers['Content-Type'] = $config['type'];
-
-            // Send server response headers
-            // Like other headers, cookies must be sent before any output from your script
-            // In InfoPotato, you should use Cookie class before $this->response()
-            if (isset($config['extra_headers']) && is_array($config['extra_headers'])) {
-                $headers = array_merge($headers, $config['extra_headers']);
-            }
-            foreach ($headers as $name => $val) {
+        }
+        
+        // If no 'status' specified, we just send out the headers and output the 'content'
+        
+        // Add other response headers
+        // The origin server will add additional headers about the response if not specified here
+        // Like other headers, cookies must be sent before any output from your script
+        // In InfoPotato, you should use Cookie class before $this->response()
+        if (isset($config['extra_headers']) && is_array($config['extra_headers'])) {
+            // Send out all the specified headers
+            foreach ($config['extra_headers'] as $name => $val) {
                 header($name.': '.$val);
             }
-            
-            // Output the uncompressed content
+        }
+        
+        if (isset($config['content'])) {
+            // Any HTTP/1.1 message containing an entity-body SHOULD include a
+            // Content-Type header field defining the media type of that body.
+            if (isset($config['type'])) {
+                header('Content-Type: '.$config['type']);
+            }
+
+            // Lastly, output the uncompressed content
             // You can use apache's mod_gzip module to compress the output if you want
-            // 1xx, 204, and 304 responses and any response to a HEAD request "MUST NOT" include a message-body
             echo $config['content'];
+        } else {
+            Logger::log_debug(APP_LOG_DIR, "Missing 'content' for response.");
         }
     }
     
