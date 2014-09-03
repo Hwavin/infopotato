abd180ba36bd119d40215a8a07d6ccebc9b2c03b
Zhou Yuan <yuanzhou19@gmail.com>
2014-09-03 11:44:23 -0400

    reordered methods in Cookie

--- system/core/cookie.php
+++ system/core/cookie.php
@@ -17,34 +17,7 @@ class Cookie {
      * @return Cookie
      */
     private function __construct() {}
-    
-    /**
-     * Deletes a cookie
-     * 
-     * @param string $name The cookie name to delete
-     * @param string $path The path of the cookie to delete
-     * @param string $domain The domain of the cookie to delete
-     * @param boolean $secure If the cookie is a secure-only cookie
-     * @return void
-     */
-    public static function delete($name, $path = NULL, $domain = NULL, $secure = NULL) {
-        // Simply assign it an expiration date that in the past
-		// If you've set the domain and path when you created the cookie, 
-		// then you should use those parameters again when deleting the cookie.
-		self::set($name, '', time() - 43200, $path, $domain, $secure);
-    }
-    
-    /**
-     * Gets a cookie value from $_COOKIE, while allowing a default value to be provided
-     * 
-     * @param string $name The name of the cookie to retrieve
-     * @param mixed $default_value If there is no cookie with the name provided, return this value instead
-     * @return mixed The value
-     */
-    public static function get($name, $default_value = NULL) {
-        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : $default_value;
-    }
-    
+
     /**
      * Set cookie
      *
@@ -76,7 +49,34 @@ class Cookie {
             setcookie($name, $value, $expires, $path, $domain, $secure, $httponly);
         }
     }
+	
+	/**
+     * Gets a cookie value from $_COOKIE, while allowing a default value to be provided
+     * 
+     * @param string $name The name of the cookie to retrieve
+     * @param mixed $default_value If there is no cookie with the name provided, return this value instead
+     * @return mixed The value
+     */
+    public static function get($name, $default_value = NULL) {
+        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : $default_value;
+    }
     
+	/**
+     * Deletes a cookie
+     * 
+     * @param string $name The cookie name to delete
+     * @param string $path The path of the cookie to delete
+     * @param string $domain The domain of the cookie to delete
+     * @param boolean $secure If the cookie is a secure-only cookie
+     * @return void
+     */
+    public static function delete($name, $path = NULL, $domain = NULL, $secure = NULL) {
+        // Simply assign it an expiration date that in the past
+		// If you've set the domain and path when you created the cookie, 
+		// then you should use those parameters again when deleting the cookie.
+		self::set($name, '', time() - 43200, $path, $domain, $secure);
+    }
+	
 }
 
 /* End of file: ./system/core/cookie.php */
